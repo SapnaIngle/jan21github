a7f3b84f4a89dfd79c33702f406157b618337e08
SapnaIngle <sapna11ingle@gmail.com>
2023-01-21 12:50:57 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
+// https://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@//github.com/SapnaIngle/jan21github.git
